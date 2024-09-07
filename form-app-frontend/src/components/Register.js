298550280f62597690efe5735d399d1eaa376793
Erick Nathan <ericknathan.dev@gmail.com>
2024-09-07 10:20:23 -0300

    Tratamentos

--- form-app-frontend/src/components/Register.js
+++ form-app-frontend/src/components/Register.js
@@ -12,6 +12,8 @@ function Register() {
     const [password, setPassword] = useState('');
     const [passwordStrength, setPasswordStrength] = useState(0);
     const [passwordError, setPasswordError] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
 
     const isPasswordStrongEnough = () => passwordStrength >= 4;
@@ -19,18 +21,26 @@ function Register() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         
+        // Check if password is strong enough
         if (!isPasswordStrongEnough()) {
             setPasswordError('A senha deve ser no mínimo forte');
-            return;
+            setSuccessMessage(''); // Clear success message if password is weak
+            setErrorMessage(''); // Clear error message if password is weak
+            return; // Exit the function if password is not strong enough
         }
+
+        // Clear error messages if password is strong
+        setPasswordError('');
+        setSuccessMessage('');
+        setErrorMessage('');
 
         try {
             await axios.post('http://localhost:4000/register', { username, email, password });
-            alert('Registro bem-sucedido!');
-            navigate('/login');
+            setSuccessMessage('Registro realizado com sucesso');
+            setTimeout(() => navigate('/login'), 2000); // Redirect after 2 seconds
         } catch (error) {
             console.error('Erro ao registrar:', error);
-            alert('Erro ao registrar.');
+            setErrorMessage('Erro ao registrar');
         }
     };
 
@@ -43,44 +53,50 @@ function Register() {
             <div className="table-register">
                 <div className="text-section">
                     <h2>REGISTRO</h2>
-                        <form onSubmit={handleSubmit}>
-                            <div class="inputs-register">
-                                <div className="input-container">
-                                    <input
-                                        type="text"
-                                        value={username}
-                                        placeholder=""
-                                        onChange={(e) => setUsername(e.target.value)}
-                                        required
-                                    />
-                                    <label htmlFor="username">Username</label>
-                                </div>
-                                <div className="input-container">
-                                    <input
-                                        type="email"
-                                        value={email}
-                                        placeholder=""
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        required
-                                    />
-                                    <label htmlFor="email">Email</label>
-                                </div>
-                                <PasswordStrengthChecker 
-                                    password={password} 
-                                    setPassword={setPassword} 
-                                    setPasswordStrength={setPasswordStrength} 
+                    <form onSubmit={handleSubmit}>
+                        <div className="inputs-register">
+                            <div className="input-container">
+                                <input
+                                    type="text"
+                                    value={username}
+                                    placeholder=""
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    required
                                 />
-                                <div class="error-container">
-                                    {passwordError && (
-                                        <p className="password-error">{passwordError}</p>
-                                    )}
-                                </div>
+                                <label htmlFor="username">Username</label>
                             </div>
+                            <div className="input-container">
+                                <input
+                                    type="email"
+                                    value={email}
+                                    placeholder=""
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <PasswordStrengthChecker 
+                                password={password} 
+                                setPassword={setPassword} 
+                                setPasswordStrength={setPasswordStrength} 
+                            />
+                            <div className="error-container">
+                                {passwordError && (
+                                    <p className="error">{passwordError}</p>
+                                )}
+                                {errorMessage && (
+                                    <p className="error">{errorMessage}</p>
+                                )}
+                                {successMessage && (
+                                    <p className="success">{successMessage}</p>
+                                )}
+                            </div>
+                        </div>
                         <div className="button-section">
                             <button type="submit">REGISTRAR</button>
                         </div>
-                        <div class="back-container">
-                            <a onClick={goToLogin} class="back-login">Voltar ao Login</a>
+                        <div className="back-container">
+                            <a onClick={goToLogin} className="back-login">Voltar ao Login</a>
                         </div>
                     </form>
                 </div>
